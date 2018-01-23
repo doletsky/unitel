810324811177bc53545d1b34d79786f46e588a78
adoletskiy <adoletsky@indigos.ru>
2018-01-24 01:45:26 +0300

    add point in breadcrump

--- bitrix/templates/.default/components/bitrix/news/cases/bitrix/news.list/.default/result_modifier.php
+++ bitrix/templates/.default/components/bitrix/news/cases/bitrix/news.list/.default/result_modifier.php
@@ -5,5 +5,10 @@ while($enum_fields = $property_enums->GetNext())
 {
   $arResult["VOLTAGE"][$enum_fields["ID"]] = $enum_fields["VALUE"];
 }
+if(array_key_exists("voltage_id", $_REQUEST)){
+    $APPLICATION->AddChainItem($arResult["VOLTAGE"][$_REQUEST["voltage_id"]]);
+}else{
+    $APPLICATION->AddChainItem($arResult["VOLTAGE"][10]);
+}
 
 ?>
