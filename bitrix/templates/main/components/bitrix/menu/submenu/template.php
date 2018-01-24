bceaa7c96e80a1387888a7d50f1c5e7a9d3be1e4
adoletskiy <adoletsky@indigos.ru>
2018-01-24 17:27:11 +0300

    light menu point

--- bitrix/templates/main/components/bitrix/menu/submenu/template.php
+++ bitrix/templates/main/components/bitrix/menu/submenu/template.php
@@ -7,6 +7,10 @@
 		foreach($arResult as $arItem):
 			if($arParams["MAX_LEVEL"] == 1 && $arItem["DEPTH_LEVEL"] > 1) 
 				continue;
+            if(substr_count($APPLICATION->GetCurDir(),"/support/shef-montazh/")>0 || substr_count($APPLICATION->GetCurDir(),"/support/shef-naladka/")>0 || substr_count($APPLICATION->GetCurDir(),"/support/obuchenie/")>0){
+                if(substr_count($arItem['LINK'],"/support/services/")>0)
+                    $arItem["SELECTED"]=1;
+            }
 		?>
 			<?if($arItem["SELECTED"]):?>
 				<li class="active"><a href="<?=$arItem["LINK"]?>" class="selected"><?=$arItem["TEXT"]?></a></li>
