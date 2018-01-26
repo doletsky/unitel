a9d4a43e79e2a7c982c5d24db4bfde1fd768af11
adoletskiy <adoletsky@indigos.ru>
2018-01-26 12:00:49 +0300

    edit text

--- include/support.php
+++ include/support.php
@@ -1,6 +1,6 @@
 <img src="img/main-widget-support-icon.svg" alt="" class="main-widget-support-icon" width="86">
 <h3 class="main-widget-title">Поддержка</h3>
-<div class="main-widget-support-text">Наши специалисты службы поддержки всегда готовы помочь вам</div>
+<div class="main-widget-support-text">Наши специалисты службы поддержки всегда готовы помочь Вам</div>
 <a data-href="#modal" href="/support/support_center/" class="main-widget-support-link">
 	<span>Сервисный центр</span>
 </a>
